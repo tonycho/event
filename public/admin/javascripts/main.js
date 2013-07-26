cb74ae0c8a553b048b668b8b2dffb5c3854270b7
tonycho <tony.cho@gmail.com>
2013-07-26 12:09:04 -0700

    added select2 for dropdown styling

--- public/admin/javascripts/main.js
+++ public/admin/javascripts/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $("a.fancybox").fancybox();
 
+    $("select.styled").select2({
+        minimumResultsForSearch:100
+    });
+
     $('.datepicker').datepicker({
         format: 'yyyy/mm/dd'
     });
